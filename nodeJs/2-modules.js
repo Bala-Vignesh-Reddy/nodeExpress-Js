6574765f3cace04d5ef6f6064cbd3301e16abbbf
reddybalavignesh9979@gmail.com <reddybalavignesh9979@gmail.com>
2023-02-26 14:51:32 +0530

    Module

---
+++ nodeJs/2-modules.js
@@ -0,0 +1,9 @@
+// Common Js, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const john = 'john'
+
+const sayHi = (name) =>{
+    console.log(`Hi ${name}`);
+}
+sayHi('peter')
+sayHi(john)
