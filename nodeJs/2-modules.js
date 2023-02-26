3f59798d680493dadd532307aeb4a096d88d378d
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-02-26 17:34:04 +0530

    About Modules

--- nodeJs/2-modules.js
+++ nodeJs/2-modules.js
@@ -2,6 +2,14 @@
 // Modules - Encapsulated Code (only share minimum)
 // console.log(module) can be used to see the details of module
 
+const names = require('./2-namesModule')
+// const {john, riya} = require('./2-namesModule')  another way to destructing it
+
+const sayHi = require('./2-utilsModule')
+
+console.log(names);
 
 sayHi('peter')
-sayHi(john)
+// sayHi(john)
+sayHi(names.john)
+sayHi(names.riya)
