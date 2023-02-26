72629bf077182e9aba7e0484a0e446dbb8ed9b28
Balavignesh <108361665+Bala-Vignesh-Reddy@users.noreply.github.com>
2023-02-26 14:47:07 +0530

    bymistake

--- nodeJs/modules.js
+++
@@ -1,9 +0,0 @@
-// Common Js, every file is module (by default)
-// Modules - Encapsulated Code (only share minimum)
-const john = 'john'
-
-const sayHi = (name) =>{
-    console.log(`Hi ${name}`);
-}
-sayHi('peter')
-sayHi(john)
