6d85ab519c3907a69dca9b7eb483c7a21062ab64
reddybalavignesh9979@gmail.com <reddybalavignesh9979@gmail.com>
2023-02-26 14:51:08 +0530

    About Global

---
+++ nodeJs/1-globals.js
@@ -0,0 +1,16 @@
+//GLOBALS - NO Window
+
+//some of the globals
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules(Common Js)
+// module     - information aout current module(file)
+// process    - infortmation about env where the program is being executed
+
+console.log(__dirname);
+// console.log(process);
+
+//  A simple function
+setInterval(() => {
+    console.log("hello World")
+}, 1000)
