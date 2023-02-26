e8ac23c3da999114aae6d193a7f33335d1634f9f
reddybalavignesh9979@gmail.com <reddybalavignesh9979@gmail.com>
2023-02-26 13:50:41 +0530

    Intro-to-node

---
+++ nodeJs/1-intro.js
@@ -0,0 +1,7 @@
+//GLOBALS - NO Window
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules(Common Js)
+// module     - information aout current module(file)
+// process    - infortmation about env where the program is being executed
