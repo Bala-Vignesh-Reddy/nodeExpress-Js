42b33b80e1304420bcd80ce345c7f687dc90a8e6
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-02-27 19:01:26 +0530

    Alternative way to export

---
+++ nodeJs/3-alternative-way.js
@@ -0,0 +1,13 @@
+// alternative way 
+module.exports.items = ['item1', 'item2']
+
+// first way 
+const person = {
+    name : 'Tru'
+}
+module.exports.singlePerson = person;
+
+//second way to write the same thing
+// module.exports.person = {
+//     name : 'Tru',
+// }
