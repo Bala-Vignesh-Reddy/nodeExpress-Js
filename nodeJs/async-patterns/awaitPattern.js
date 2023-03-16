71f4eae4712c72fce8b1be7253ada8ae8c0d7738
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-16 15:01:35 +0530

    Await pattern using promises

--- nodeJs/async-patterns/awaitPattern.js
+++ nodeJs/async-patterns/awaitPattern.js
@@ -1,18 +1,40 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs')
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf8', (err, data) =>{
-      if(err){
-        reject(err)
-      } else{
-        resolve(data)
-      }
-    })
-  })
+//instead of this we can also write 
+// const { readFile, writeFile } = require('fs').promises
+// this will also work 
+
+const util = require('util')
+const readFilePromise  = util.promisify(readFile)
+const writeFilePromise  = util.promisify(writeFile)
+
+
+const start = async() => {
+    try {
+        const first = await readFilePromise('./contents/first.txt', 'utf8')
+        const second = await readFilePromise('./contents/second.txt', 'utf8')
+        await writeFilePromise('./contents/result-filePromise.txt', `This is awesome: ${first} ${second}`)
+        console.log(first, second)
+  } catch (error) {
+      console.log(error);
+  }
 }
 
-getText('../contents/first.txt')
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err))
+start()
 
+
+      // const getText = (path) => {
+      //   return new Promise((resolve, reject) => {
+      //     readFile(path, 'utf8', (err, data) =>{
+      //       if(err){
+      //         reject(err)
+      //       } else{
+      //         resolve(data)
+      //       }
+      //     })
+      //   })
+      // }
+      
+      //getText('../contents/first.txt')
+       // .then((result) => console.log(result))
+        //.catch((err) => console.log(err))
