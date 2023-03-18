0b399a218e497c0517574d5050bb97b7bdca93b8
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-18 11:50:07 +0530

    testing

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,15 +1,1 @@
-// STREAMS
-// This are used to read or write sequentially
-// 4 types of stream
-// Writeable for writing
-// Readable for reading
-// Duplex for both
-// Transform to modify
-
-const { createReadStream } = require('fs')
-
-const stream = createReadStream('./contents/big.txt');
-
-stream.on('data', (result) => {
-    console.log(result)
-})
+console.log("hello Node!");
