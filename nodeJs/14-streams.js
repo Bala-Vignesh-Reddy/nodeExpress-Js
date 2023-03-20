4e9bde4a6c8883c018e21b19bc16796e54e8cf69
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-20 22:09:21 +0530

    Streams

--- nodeJs/14-streams.js
+++ nodeJs/14-streams.js
@@ -8,9 +8,18 @@
 
 const { createReadStream } = require('fs')
 
-const stream = createReadStream('./contents/big.txt');
+const stream = createReadStream('./contents/big.txt', {highWaterMark: 90000, encoding: 'utf8'});
+
+// default size of buffer is 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./contents/big.txt', { highWaterMark: 90000})
+// const stream = createReadStream('../contents/big.txt', { encoding: 'utf8'})  
 
 stream.on('data', (result) => {
     console.log(result)
 })
     
+stream.on('error', (err) => {
+    console.log(err);
+})
