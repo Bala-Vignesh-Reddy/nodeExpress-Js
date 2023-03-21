de24b8a6266be8ec85d467a298d6f04eb6680fb3
Balavignesh <108361665+Bala-Vignesh-Reddy@users.noreply.github.com>
2023-03-21 20:55:58 +0530

    Update 13-createBigFile.js

--- nodeJs/13-createBigFile.js
+++ nodeJs/13-createBigFile.js
@@ -1,4 +1,4 @@
-//creation of big text
+//creation of big text for streams
 const { writeFileSync } = require('fs')
 for(let i=0; i<10000; i++){
     writeFileSync('./contents/big.txt', `hello world ${i}\n`, {flag: 'a'})
