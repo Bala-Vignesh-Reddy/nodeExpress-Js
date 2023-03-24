580375ddcf0af54b0de47cd3e1c791c757388e1c
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-24 23:17:22 +0530

    first-server

--- expressJs/my-express-server/server.js
+++ expressJs/my-express-server/server.js
@@ -4,6 +4,18 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', function (request, response) {
+  response.send("Hello");
+})
+
+app.get('/contact', (req, res) => {
+  res.send("contact me at: alphapython@gmail.com")
+})
+
+app.get('/about', (req, res) => {
+  res.send('My name is AlphaPython.')
+  re.send()
+})
 app.listen(3000, function(){
   console.log("Server Started on port 3000");
 })
