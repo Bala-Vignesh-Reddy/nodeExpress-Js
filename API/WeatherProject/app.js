e8896344ca25a11b30e49b7b6ac93ed4fe62336d
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-27 22:54:26 +0530

    updated weather project

--- API/WeatherProject/app.js
+++ API/WeatherProject/app.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.get('/', function(req, res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Kalol&appid=e58d68d3ca1a9f8e2a64f9454ab40685&units=metric#"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e58d68d3ca1a9f8e2a64f9454ab40685&units=metric#"
     https.get(url, function(response){
         console.log(response.statusCode);
 
@@ -24,11 +24,16 @@ app.get('/', function(req, res){
 
             const temp = weatherData.main.temp; //if we have complex json then we can use the extension installed in chrome
             const description = weatherData.weather[0].description;
-            console.log(description);
+            const icon = weatherData.weather[0].icon;
+            const imgURL = "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
+            res.write("<p>The weather is currently " + description + "</p>")
+            res.write("<h1>The temperature in London is " + temp + " degree Celcius.</h1>");
+            res.write("<img src="+imgURL+">")
+            res.send()
         })
     })
 
-    res.send("Server is up and running");
+    // res.send("Server is up and running");
 })
 
 
