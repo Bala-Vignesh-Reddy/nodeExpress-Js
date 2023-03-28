b31a2e27e204e38188b7608a0da40ffc627dde0a
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-28 22:42:36 +0530

    Js

--- API/WeatherProject/app.js
+++ API/WeatherProject/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.get('/', function(req, res){
+app.use(bodyParser.urlencoded({extended: true}))
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e58d68d3ca1a9f8e2a64f9454ab40685&units=metric#"
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    const query = req.body.cityName
+    const appid = "e58d68d3ca1a9f8e2a64f9454ab40685"
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" + appid + "&units=" + unit
     https.get(url, function(response){
-        console.log(response.statusCode);
+        // console.log(response.statusCode);
 
         response.on('data', function(data){
             //if we log data then we will get in hexadecimal format
@@ -27,7 +36,7 @@ app.get('/', function(req, res){
             const icon = weatherData.weather[0].icon;
             const imgURL = "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
             res.write("<p>The weather is currently " + description + "</p>")
-            res.write("<h1>The temperature in London is " + temp + " degree Celcius.</h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degree Celcius.</h1>");
             res.write("<img src="+imgURL+">")
             res.send()
         })
@@ -40,5 +49,5 @@ app.get('/', function(req, res){
 
 
 app.listen(3000, function(){
-    console.log("Server is runnign on port 3000.")
+    console.log("Server is running on port 3000.")
 })
