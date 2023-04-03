87d432ef74253d800f990593d812758fb667d2d4
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-04-04 00:02:23 +0530

    tutorial

--- expressJs/express-tutorial/app.js
+++ expressJs/express-tutorial/app.js
@@ -1,7 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log("Server is up and running")
+//  console.log("Server is up and running")
+  console.log(req.url)
+  res.writeHead(200, {'content-type': 'text/html'})
+  res.write('<h1>Home page</h1>')
 }) 
 
 
