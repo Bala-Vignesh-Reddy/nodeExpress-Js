7653b3f93f108968f27889b412f96ecd0f2e0dc2
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-06-22 18:46:23 +0530

    Js

--- Projects/QR-Generator/index.js
+++ Projects/QR-Generator/index.js
@@ -23,6 +23,7 @@ inquirer
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
+      //
     } else {
       // Something else went wrong
     }
